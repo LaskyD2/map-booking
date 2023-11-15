8c9f228fce80e7e38e07c26e254f11897792ec32
Денис Макаров <35608039+LaskyD2@users.noreply.github.com>
2023-11-15 11:56:34 +0300

    Add files via upload

---
+++ map-booking/js/lang.js
@@ -0,0 +1,18 @@
+export const LANG_SETTING = {
+    ru: {
+        SelectDate: `Выберите дату`,
+        From: `От`,
+        NoPlaces: `Нет свободных мест`,
+        Apartments: `квартиры`,
+        Busy: `Занято`,
+
+    },
+    en: {
+        SelectDate: `Select date`,
+        From: `From`,
+        NoPlaces: `No free places`,
+        Apartments: `apart.`,
+        Busy: `Busy`,
+    },
+};
+
---
+++ map-booking/js/module/rooms-list.js
@@ -0,0 +1,81 @@
+import {templateIconContent, templateBalloonContent, templateClusterContent} from "../views/template.js";
+import { placemarks } from '../model/placemarks.js';
+import { geoObjects, clusterer, map } from '../map.js';
+
+
+export const roomsList = (roomsFb, roomList) => {
+
+    let minPriceMarker;
+    let placemarksRoster = placemarks();
+
+    roomsFb.forEach((roomfb) => {
+        geoObjects.forEach((item) => {
+            if (item.properties.get('roomTypes').includes(roomfb.id)) {
+                item.properties._data.price = roomfb.price;
+            }
+        })
+    })
+
+    placemarksRoster.forEach((item, index) => {
+        let roomPrice = [], roomsObj = [];
+        let roomsMarker = item.roomTypes;
+
+        roomList.forEach((room) => {
+            if (roomsMarker.indexOf(room.id) !== -1) {
+                roomsObj.push(room);
+            }
+        })
+
+        if (roomsFb) {
+            roomsFb.forEach((roomfb) => {
+                roomsObj.forEach((roomObj) => {
+                    if (roomfb.id === roomObj.id) {
+                        roomPrice.push(roomfb.price);
+                        roomObj.price = roomfb.price;
+                    }
+                })
+            })
+        }
+
+
+        roomsObj.sort((a, b) => {
+            if (a.price == null)
+                return '1';
+            else if (b.price == null)
+                return '-1';
+            else
+                return a.price - b.price;
+        });
+
+        minPriceMarker = roomPrice.length ? Math.min.apply(null, roomPrice) : null;
+
+        let address = geoObjects[index].properties.get('address');
+        let idElement = geoObjects[index].properties.get('id');
+
+        geoObjects[index].properties.set('iconContent', templateIconContent('price', minPriceMarker, idElement, roomsObj.length));
+        geoObjects[index].properties.set('balloonContent', templateBalloonContent(address, roomsObj));
+
+    })
+
+
+    function changeColorClusters() {
+        let clusters = clusterer.getClusters();
+        clusters.forEach((cluster) => {
+            let chechPrice = false;
+            cluster.properties.get('geoObjects').forEach((item) => {
+                if (item.properties.get('price') && chechPrice === false) {
+                    chechPrice = true;
+                    cluster.options.set('clusterIconColor', '#5a9b3b');
+                }
+            })
+            // cluster.options.set('clusterIconLayout', templateClusterContent(cluster.properties.get('geoObjects').length));
+        });
+    }
+    changeColorClusters();
+    map.events.add('boundschange', function(event) {
+        if (event.get('newZoom') !== event.get('oldZoom')) {
+            changeColorClusters()
+        }
+    });
+
+}
